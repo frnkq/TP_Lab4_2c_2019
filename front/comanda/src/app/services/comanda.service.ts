5ebea11ddb2955508dc885cfffed5b1185d2cba3
Franco Canevali <mail@francocanevali.com>
2019-10-16 23:30:43 -0300

    working fully on prod

--- front/comanda/src/app/services/comanda.service.ts
+++ front/comanda/src/app/services/comanda.service.ts
@@ -1,12 +1,12 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ComandaService {
-  host = "http://localhost:8080/";
+  host = "http://comandapi.francocanevali.com/";
   endpoints = {
     auth: {
       login: "auth/login"
@@ -17,9 +17,15 @@ export class ComandaService {
   public Login(username: string, password: string): Observable<any>
   {
     let endpoint = this.host+this.endpoints.auth.login;
+    console.log("endpoint: ", endpoint);
+     let header = new HttpHeaders();
+    header.set("Access-Control-Allow-Origin", "*");
     return this.http.post(endpoint,
        JSON.stringify({username: username, password:password}),
-       {responseType: 'text'}
+       {
+         responseType: 'text',
+        headers: header
+        }
        ); 
   }
 }
